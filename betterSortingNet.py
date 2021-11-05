66850f82472034023301d3b64582227930e54743
Andrew Cummings <acummings@egnyte.com>
2021-11-05 15:22:33 -0700

    working net search

--- betterSortingNet.py
+++ betterSortingNet.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 import math
-import numpy as np
-import numba as nb
+from bit_array import BitArray
 
 NEIGHBOR_COUNT = 8
 TOTAL_PAIRS = (NEIGHBOR_COUNT - 1) * NEIGHBOR_COUNT // 2
@@ -10,13 +9,27 @@ PAIR_PERCENT = 1 / TOTAL_PAIRS
 OUTPUT_SPACE_SIZE = 2 ** NEIGHBOR_COUNT
 MAX_SWAPS = 19
 
-# short-circuiting replacement for np.any()
-@nb.jit(nopython=True)
-def sc_any(array):
-    for x in array.flat:
-        if x:
-            return True
-    return False
+CORRECT_SWAPS = [
+	(0, 4),
+	(1, 5),
+	(2, 6),
+	(3, 7),
+	(0, 2),
+	(1, 3),
+	(4, 6),
+	(5, 7),
+	(2, 4),
+	(3, 5),
+	(0, 1),
+	(2, 3),
+	(4, 5),
+	(6, 7),
+	(1, 4),
+	(3, 6),
+	(1, 2),
+	(3, 4),
+	(5, 6),
+]
 
 def isOutputAllowed(output: int):
 	neighbors = [0] * NEIGHBOR_COUNT
@@ -41,174 +54,6 @@ def isOutputAllowed(output: int):
 		if (newState != wantedState): return False
 
 	return True
-
-class BitArray:
-	def CHUNK_FORMAT(bits: int) -> str:
-		return "{0:0" + str(bits) + "b}"
-
-	def __init__(self, size: int, value: bool=False, chunks: np.ndarray=None, chunk_type=None):
-		if chunk_type is None:
-			chunk_type = np.uint8
-		self.chunk_size = np.dtype(chunk_type).itemsize * 8
-
-		self.size = size
-		self.chunk_count = size // self.chunk_size
-		if (size % self.chunk_size != 0):
-			self.chunk_count += 1
-		
-		if chunks is None:
-			self.chunks = np.empty(self.chunk_count, dtype=chunk_type)
-			self.chunks[:] = 0xFFFFFFF if value else 0
-		else:
-			self.chunks = chunks
-
-	def copy(self) -> BitArray:
-		c = BitArray.__new__(BitArray)
-		c.chunk_size = self.chunk_size
-		c.size = self.size
-		c.chunk_count = self.chunk_count
-		c.chunks = self.chunks.copy() 
-		return c
-
-	def allZero(self) -> bool:
-		extraBits = self.size - (self.chunk_count - 1) * self.chunk_size
-		return (self.chunks[-1] & ((1 << extraBits) - 1)) == 0 and (self.chunks[:-1] == 0).all()
-
-	# this will NOT handle size % self.chunk_size != 0 correctly
-	def fastAllZero(self) -> bool:
-		return not sc_any(self.chunks)
-
-	# this will NOT handle size % self.chunk_size != 0 correctly
-	def hash(self) -> bytes:
-		return self.chunks.tobytes() 
-
-	def canOp(self, other: BitArray) -> bool:
-		return type(other) is BitArray and self.size == other.size and self.chunk_size == other.chunk_size
-
-	def __or__(self, other: BitArray) -> BitArray:
-		out = self.copy()
-		out |= other
-		return out
-
-	def __and__(self, other: BitArray) -> BitArray:
-		out = self.copy()
-		out &= other
-		return out
-
-	def __xor__(self, other: BitArray) -> BitArray:
-		out = self.copy()
-		out ^= other
-		return out
-
-	def __invert__(self) -> BitArray:
-		out = self.copy()
-		out.invert()
-		return out
-
-	def __rshift__(self, amount: int) -> BitArray:
-		out = self.copy()
-		out >>= amount
-		return out
-
-	def __lshift__(self, amount: int) -> BitArray:
-		out = self.copy()
-		out <<= amount
-		return out
-
-	def __ior__(self, other: BitArray) -> BitArray:
-		# assert self.canOp(other)
-		np.bitwise_or(self.chunks, other.chunks, out=self.chunks)
-		return self
-
-	def __iand__(self, other: BitArray) -> BitArray:
-		# assert self.canOp(other)
-		np.bitwise_and(self.chunks, other.chunks, out=self.chunks)
-		return self
-
-	def __ixor__(self, other: BitArray) -> BitArray:
-		# assert self.canOp(other)
-		np.bitwise_xor(self.chunks, other.chunks, out=self.chunks)
-		return self
-
-	# this will NOT handle size % self.chunk_size != 0 correctly
-	def __irshift__(self, amount: int) -> BitArray:
-		if amount == 0: return self
-		if amount >= self.size:
-			self.chunks[:] = 0
-			return self
-		# assert amount > 0
-
-		if amount < self.chunk_size:
-			rolled = np.empty_like(self.chunks)
-			rolled[1:] = self.chunks[:-1]
-			rolled[0] = 0 
-			np.right_shift(rolled, self.chunk_size - amount, out=rolled) # get rollover bits from adjacent chunks
-
-			np.left_shift(self.chunks, amount, out=self.chunks) # main shift of each byte
-			np.bitwise_or(self.chunks, rolled, out=self.chunks)
-			return self
-		else:
-			rolls = amount // self.chunk_size
-			self.chunks[rolls:] = self.chunks[:-rolls]
-			self.chunks[:rolls] = 0
-			return self.__irshift__(amount - rolls * self.chunk_size) # extra remaining shift
-
-	# this will NOT handle size % self.chunk_size != 0 correctly
-	def __ilshift__(self, amount: int) -> BitArray:
-		if amount == 0: return self
-		if amount >= self.size:
-			self.chunks[:] = 0
-			return self
-		# assert amount > 0
-
-		if amount < self.chunk_size:
-			rolled = np.empty_like(self.chunks)
-			rolled[:-1] = self.chunks[1:]
-			rolled[-1] = 0 
-			np.left_shift(rolled, self.chunk_size - amount, out=rolled)
-
-			np.right_shift(self.chunks, amount, out=self.chunks)
-			np.bitwise_or(self.chunks, rolled, out=self.chunks)
-			return self
-		else:
-			rolls = amount // self.chunk_size
-			self.chunks[:-rolls] = self.chunks[rolls:]
-			self.chunks[-rolls:] = 0
-			return self.__ilshift__(amount - rolls * self.chunk_size)
-
-	def invert(self):
-		np.invert(self.chunks, out=self.chunks)
-
-	def __getitem__(self, key: int) -> bool:
-		# assert key < self.size
-		byteIndex, bitIndex = self.indices(key)
-		return bool((self.chunks[byteIndex] >> bitIndex) & 1)
-
-	def __setitem__(self, key: int, value: bool):
-		# assert key < self.size
-		byteIndex, bitIndex = self.indices(key)
-		if value:
-			self.chunks[byteIndex] = (1 << bitIndex) | self.chunks[byteIndex]
-		else:
-			self.chunks[byteIndex] = ~(1 << bitIndex) & self.chunks[byteIndex]
-
-	def indices(self, key: int):
-		byteIndex = key // self.chunk_size
-		bitIndex = key - byteIndex * self.chunk_size
-		return (byteIndex, bitIndex)
-
-	def __str__(self) -> str:
-		sep = ""
-		out_str = sep.join(list(BitArray.CHUNK_FORMAT(self.chunk_size).format(v)[::-1] for v in self.chunks[:-1]))
-		if (self.size > self.chunk_size):
-			out_str += sep
-
-		extra_bits = (self.size % self.chunk_size) or self.chunk_size
-		return out_str + BitArray.CHUNK_FORMAT(extra_bits).format(self.chunks[-1])[::-1][:extra_bits]
-
-	def __eq__(self, other: BitArray) -> bool:
-		if type(other) is not BitArray: return False
-		return np.array_equal(self.chunks, other.chunks)
 
 class OutputSpace:
 	NEIGHBOR_LOW_MASKS = {}
@@ -355,6 +200,7 @@ def findSwaps(outputSpace: OutputSpace, swaps: list[tuple[int]], swaps_count: in
 
 	if best_explored is not None:
 		explored_output_spaces[spaceHash] = best_explored
+		return best_explored
 	elif spaceHash in explored_output_spaces:
 		explored = explored_output_spaces[spaceHash]
 		if (explored.height < MAX_SWAPS - swaps_count):
@@ -362,30 +208,10 @@ def findSwaps(outputSpace: OutputSpace, swaps: list[tuple[int]], swaps_count: in
 	else:
 		explored_output_spaces[spaceHash] = ExploredSpace(MAX_SWAPS - swaps_count, None, False)
 
-correct_swaps = [
-	(0, 4),
-	(1, 5),
-	(2, 6),
-	(3, 7),
-	(0, 2),
-	(1, 3),
-	(4, 6),
-	(5, 7),
-	(2, 4),
-	(3, 5),
-	(0, 1),
-	(2, 3),
-	(4, 5),
-	(6, 7),
-	(1, 4),
-	(3, 6),
-	(1, 2),
-	(3, 4),
-	(5, 6),
-]
+	return None
 
 testSpace = OutputSpace()
-for i, j in correct_swaps:
+for i, j in CORRECT_SWAPS:
 	testSpace.cmpSwap(i, j)
 
 if testSpace.isAllowed():
@@ -393,34 +219,17 @@ if testSpace.isAllowed():
 else:
 	print("Failed sanity check!")
 
-depth = 0
+swaps_count = 0
 swaps = [None] * MAX_SWAPS
-start_swaps = correct_swaps[:10]
+start_swaps = CORRECT_SWAPS[:10]
 outputSpace = OutputSpace()
 for i, j in start_swaps:
 	outputSpace.cmpSwap(i, j)
-	swaps[depth] = (i, j)
-	depth += 1
+	swaps[swaps_count] = (i, j)
+	swaps_count += 1
 
 results: dict[str, ExploredSpace] = {}
 count = ExploredCount(0)
-findSwaps(outputSpace, swaps, depth, results, count)
+bestPath = findSwaps(outputSpace, swaps, swaps_count, results, count)
 print("Explored (final)", count.count)
-print([result for result in results.values() if result.success and result.height > 7])
-
-# s = 64
-# t1 = BitArray(s, chunks=(np.random.rand(s // 8) * (2 ** 8)).astype(np.uint8), chunk_type=np.uint8)
-# t2 = BitArray(s, chunk_type=np.uint16)
-# for i in range(s):
-# 	t2[i] = t1[i]
-
-# for k in range(s):
-# 	t1[i] = not(t1[i])
-# 	t2[i] = not(t2[i])
-# 	if str(t1) != str(t2):
-# 		print("SAD")
-
-# for k in range(s):
-# 	if str(t1 << k) != str(t2 << k):
-# 		print("SAD")
-# 		break
+print("Best =", start_swaps + bestPath.swaps)
---
+++ bit_array.py
@@ -0,0 +1,180 @@
+from __future__ import annotations
+import numpy as np
+import numba as nb
+
+# short-circuiting replacement for np.any()
+@nb.jit(nopython=True)
+def sc_any(array):
+    for x in array.flat:
+        if x:
+            return True
+    return False
+
+
+class BitArray:
+	def CHUNK_FORMAT(bits: int) -> str:
+		return "{0:0" + str(bits) + "b}"
+
+	def __init__(self, size: int, value: bool=False, chunks: np.ndarray=None, chunk_type=None):
+		if chunk_type is None:
+			chunk_type = np.uint8
+		self.chunk_size = np.dtype(chunk_type).itemsize * 8
+
+		self.size = size
+		self.chunk_count = size // self.chunk_size
+		if (size % self.chunk_size != 0):
+			self.chunk_count += 1
+		
+		if chunks is None:
+			self.chunks = np.empty(self.chunk_count, dtype=chunk_type)
+			self.chunks[:] = 0xFFFFFFF if value else 0
+		else:
+			self.chunks = chunks
+
+	def copy(self) -> BitArray:
+		c = BitArray.__new__(BitArray)
+		c.chunk_size = self.chunk_size
+		c.size = self.size
+		c.chunk_count = self.chunk_count
+		c.chunks = self.chunks.copy() 
+		return c
+
+	def allZero(self) -> bool:
+		extraBits = self.size - (self.chunk_count - 1) * self.chunk_size
+		return (self.chunks[-1] & ((1 << extraBits) - 1)) == 0 and (self.chunks[:-1] == 0).all()
+
+	# this will NOT handle size % self.chunk_size != 0 correctly
+	def fastAllZero(self) -> bool:
+		return not sc_any(self.chunks)
+
+	# this will NOT handle size % self.chunk_size != 0 correctly
+	def hash(self) -> bytes:
+		return self.chunks.tobytes() 
+
+	def canOp(self, other: BitArray) -> bool:
+		return type(other) is BitArray and self.size == other.size and self.chunk_size == other.chunk_size
+
+	def __or__(self, other: BitArray) -> BitArray:
+		out = self.copy()
+		out |= other
+		return out
+
+	def __and__(self, other: BitArray) -> BitArray:
+		out = self.copy()
+		out &= other
+		return out
+
+	def __xor__(self, other: BitArray) -> BitArray:
+		out = self.copy()
+		out ^= other
+		return out
+
+	def __invert__(self) -> BitArray:
+		out = self.copy()
+		out.invert()
+		return out
+
+	def __rshift__(self, amount: int) -> BitArray:
+		out = self.copy()
+		out >>= amount
+		return out
+
+	def __lshift__(self, amount: int) -> BitArray:
+		out = self.copy()
+		out <<= amount
+		return out
+
+	def __ior__(self, other: BitArray) -> BitArray:
+		# assert self.canOp(other)
+		np.bitwise_or(self.chunks, other.chunks, out=self.chunks)
+		return self
+
+	def __iand__(self, other: BitArray) -> BitArray:
+		# assert self.canOp(other)
+		np.bitwise_and(self.chunks, other.chunks, out=self.chunks)
+		return self
+
+	def __ixor__(self, other: BitArray) -> BitArray:
+		# assert self.canOp(other)
+		np.bitwise_xor(self.chunks, other.chunks, out=self.chunks)
+		return self
+
+	# this will NOT handle size % self.chunk_size != 0 correctly
+	def __irshift__(self, amount: int) -> BitArray:
+		if amount == 0: return self
+		if amount >= self.size:
+			self.chunks[:] = 0
+			return self
+		# assert amount > 0
+
+		if amount < self.chunk_size:
+			rolled = np.empty_like(self.chunks)
+			rolled[1:] = self.chunks[:-1]
+			rolled[0] = 0 
+			np.right_shift(rolled, self.chunk_size - amount, out=rolled) # get rollover bits from adjacent chunks
+
+			np.left_shift(self.chunks, amount, out=self.chunks) # main shift of each byte
+			np.bitwise_or(self.chunks, rolled, out=self.chunks)
+			return self
+		else:
+			rolls = amount // self.chunk_size
+			self.chunks[rolls:] = self.chunks[:-rolls]
+			self.chunks[:rolls] = 0
+			return self.__irshift__(amount - rolls * self.chunk_size) # extra remaining shift
+
+	# this will NOT handle size % self.chunk_size != 0 correctly
+	def __ilshift__(self, amount: int) -> BitArray:
+		if amount == 0: return self
+		if amount >= self.size:
+			self.chunks[:] = 0
+			return self
+		# assert amount > 0
+
+		if amount < self.chunk_size:
+			rolled = np.empty_like(self.chunks)
+			rolled[:-1] = self.chunks[1:]
+			rolled[-1] = 0 
+			np.left_shift(rolled, self.chunk_size - amount, out=rolled)
+
+			np.right_shift(self.chunks, amount, out=self.chunks)
+			np.bitwise_or(self.chunks, rolled, out=self.chunks)
+			return self
+		else:
+			rolls = amount // self.chunk_size
+			self.chunks[:-rolls] = self.chunks[rolls:]
+			self.chunks[-rolls:] = 0
+			return self.__ilshift__(amount - rolls * self.chunk_size)
+
+	def invert(self):
+		np.invert(self.chunks, out=self.chunks)
+
+	def __getitem__(self, key: int) -> bool:
+		# assert key < self.size
+		byteIndex, bitIndex = self.indices(key)
+		return bool((self.chunks[byteIndex] >> bitIndex) & 1)
+
+	def __setitem__(self, key: int, value: bool):
+		# assert key < self.size
+		byteIndex, bitIndex = self.indices(key)
+		if value:
+			self.chunks[byteIndex] = (1 << bitIndex) | self.chunks[byteIndex]
+		else:
+			self.chunks[byteIndex] = ~(1 << bitIndex) & self.chunks[byteIndex]
+
+	def indices(self, key: int):
+		byteIndex = key // self.chunk_size
+		bitIndex = key - byteIndex * self.chunk_size
+		return (byteIndex, bitIndex)
+
+	def __str__(self) -> str:
+		sep = ""
+		out_str = sep.join(list(BitArray.CHUNK_FORMAT(self.chunk_size).format(v)[::-1] for v in self.chunks[:-1]))
+		if (self.size > self.chunk_size):
+			out_str += sep
+
+		extra_bits = (self.size % self.chunk_size) or self.chunk_size
+		return out_str + BitArray.CHUNK_FORMAT(extra_bits).format(self.chunks[-1])[::-1][:extra_bits]
+
+	def __eq__(self, other: BitArray) -> bool:
+		if type(other) is not BitArray: return False
+		return np.array_equal(self.chunks, other.chunks)
